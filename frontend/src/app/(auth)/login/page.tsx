c23dbbcc87875e0dc2d46f87302ebf6fa72e7e5a
24LP6733 24LP6733 <24LP6733@G302-CC0097.local>
2025-05-07 12:03:33 +0800

    login layout

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+import Frame from "@/components/loginCom/Frame";
+import React from "react";
+
+const page = () => {
+  return <div className="text-[100px] font-extrabold">hello</div>;
+};
+
+export default page;
