db5ea61af3bb836d7740f12a6bc8c20167cdd47c
24LP6733 24LP6733 <24LP6733@G302-CC0097.local>
2025-05-13 11:05:12 +0800

    signup and signin BE

--- backend/src/controllers/auth/sign-in.controller.ts
+++ backend/src/controllers/auth/sign-in.controller.ts
@@ -5,23 +5,34 @@ import jwt from "jsonwebtoken";
 import { configDotenv } from "dotenv";
 import { secret_key } from "../../utils/env";
 configDotenv();
+
 export const signIn = async (req: Request, res: Response) => {
-  const { email, password, name } = req.body;
+  const { email, password } = req.body;
+  console.log(email, password, "FROM BODY");
+
   try {
     const user = await prisma.user.findUnique({
       where: {
         email: email,
       },
     });
-    if (!user) return res.send({ message: "user not found" });
-    const isMatch = compareSync(password, user.password);
-    if (!isMatch) return res.send({ message: "Email or password incorrect" });
-    const token = jwt.sign(user, secret_key as string, { expiresIn: 3600 });
+    console.log(user, "USER");
 
-    return res.cookie("token", token, {
+    if (!user) return res.send({ message: "user not found" });
+    const isMatch = bcrypt.compareSync(password, user.password);
+    console.log(isMatch, "PASSWORD MATCH");
+
+    if (!isMatch) return res.send({ message: "Email or password incorrect" });
+    const token = jwt.sign(user, process.env.Secret as string, {
+      expiresIn: 3600,
+    });
+    console.log(token, "TOKEN");
+
+    res.cookie("token", token, {
       maxAge: 60 * 1000 * 10,
       secure: false,
     });
+    return res.send({ success: true, token: token });
   } catch (error) {
     res.send({
       success: false,
