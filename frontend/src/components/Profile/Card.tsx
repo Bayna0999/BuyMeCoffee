a1d10e944c8ac37a0064f94736b2e046e8ef9339
24LP6733 24LP6733 <24LP6733@G302-CC0097.local>
2025-05-16 09:50:54 +0800

    hha

--- frontend/src/components/Profile/Card.tsx
+++ frontend/src/components/Profile/Card.tsx
@@ -1,13 +1,223 @@
 "use client";
-import React from "react";
 
-var FileReader: {
-  new (): FileReader;
-  prototype: FileReader;
-  readonly EMPTY: 0;
-  readonly LOADING: 1;
-  readonly DONE: 2;
-};
-export const Card = () => {
-  return <div></div>;
-};
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@radix-ui/react-select";
+import axios from "axios";
+const formSchema = z.object({
+  country: z.string({
+    message: "Select country to continue",
+  }),
+  firstname: z.string().min(2, {
+    message: "First name must match",
+  }),
+  lastname: z.string().min(2, {
+    message: "Last name must match",
+  }),
+  cardnumber: z.string().min(2, {
+    message: "Invalid card number",
+  }),
+  expired: z.string().min(2, {
+    message: "Invalid month",
+  }),
+  year: z.string().min(2, {
+    message: "Invalid year",
+  }),
+  CVC: z.string().min(2, {
+    message: "Invalid number",
+  }),
+});
+export function Card() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      country: "",
+    },
+  });
+  const [url, setUrl] = useState(null);
+  const handlePreview = (files) => {
+    console.log(files);
+    setUrl(files.target.files[0]);
+  };
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    console.log(url, "this");
+  }
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `https://restcountries.com/v3.1/all?fields=name`
+    );
+    setData(res.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data, "dataa");
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="country"
+          render={({ field }) => (
+            <FormItem {...field}>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Country</SelectLabel>
+                      {data?.map((val: string, index: number) => {
+                        return (
+                          <SelectItem key={index} value={val.name.common}>
+                            {val}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-[10px] w-[510px]">
+          {" "}
+          <FormField
+            control={form.control}
+            name="firstname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-[250px]"
+                    {...field}
+                    placeholder="Enter your name here"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Lastname</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-[250px]"
+                    {...field}
+                    placeholder="Enter your name here"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="cardnumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Enter card number</FormLabel>
+              <FormControl>
+                <Input placeholder="XXXX-XXXX-XXXX-XXXX" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-[16px]">
+          <FormField
+            control={form.control}
+            name="cardnumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter card number</FormLabel>
+                <FormControl>
+                  <Input
+                    type="date"
+                    placeholder="XXXX-XXXX-XXXX-XXXX"
+                    {...field}
+                    className="w-[160px] h-[36px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="cardnumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter card number</FormLabel>
+                <FormControl>
+                  <Input
+                    type="date"
+                    placeholder="XXXX-XXXX-XXXX-XXXX"
+                    {...field}
+                    className="w-[160px] h-[36px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="cardnumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter card number</FormLabel>
+                <FormControl>
+                  <Input
+                    type="date"
+                    placeholder="XXXX-XXXX-XXXX-XXXX"
+                    {...field}
+                    className="w-[160px] h-[36px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,51 +1,3 @@
-// "use client";
-
-// import { z } from "zod"
-
-// const formSchema = z.object({
-//   username: z.string().min(2).max(50),
-// })
-
-// export function Profile() {
-//   return (
-//     <Card className="w-[550px]">
-//       <CardHeader>
-//         <CardTitle>Complete your profile page</CardTitle>
-//       </CardHeader>
-//       <CardContent>
-//         <form>
-//           <div className="grid w-full items-center gap-4">
-
-//             <div className="flex flex-col space-y-1.5">
-//               <Label htmlFor="framework">Name</Label>
-//               <Input
-//                 className="w-[510px] h-[40px]"
-//                 placeholder="Enter your name here"
-//               ></Input>
-//             </div>
-//             <div className="flex flex-col space-y-1.5">
-//               <Label htmlFor="framework">Framework</Label>
-//               <textarea
-//                 className="h-[131px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex  w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-//                 placeholder="Write about yourself here"
-//               ></textarea>
-//             </div>
-//             <div className="flex flex-col space-y-1.5">
-//               <Label htmlFor="framework">Framework</Label>
-//               <Input
-//                 className="w-[510px] h-[40px]"
-//                 placeholder="http//:"
-//               ></Input>
-//             </div>
-//           </div>
-//         </form>
-//       </CardContent>
-//       <CardFooter className="flex justify-end">
-//         <Button variant="outline">Continue</Button>
-//       </CardFooter>
-//     </Card>
-//   );
-// }
 "use client";
 import ReactFileReader from "react-file-reader";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -81,25 +33,25 @@ const formSchema = z.object({
     message: "Please enter a social link",
   }),
 });
-function onSubmit(values: z.infer<typeof formSchema>) {
-  console.log(values);
-}
 
 export function Profile() {
-  // ...
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
   });
-
-  const [url, setUrl] = useState("");
-
-  const handleFiles = (files) => {
+  const [url, setUrl] = useState(null);
+  const handlePreview = (files) => {
     console.log(files);
-    setUrl(files.base64);
+    setUrl(files.target.files[0]);
   };
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    console.log(url, "this");
+  }
+  console.log(url, "url");
+  const imageUrl = url ? URL.createObjectURL(url as any) : null;
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -112,37 +64,21 @@ export function Profile() {
               <FormControl>
                 <div className=" size-[160px] border-[1px] border-dashed rounded-full flex justify-center items-center relative">
                   <input
-                    hidden
-                    className="size-[160px] rounded-full flex justify-center items-center"
+                    className="size-[160px] rounded-full flex justify-center items-center opacity-0 absolute z-1"
                     type="file"
                     id="files"
                     {...field}
+                    onChange={(event) => {
+                      field.onChange(event);
+                      handlePreview(event);
+                    }}
                   />
-                  <Camera className="flex absolute size-[30px]" />
-                  {/* <ReactFileReader
-                    as={Button}
-                    fileTypes={[".png", ".jpg"]}
-                    base64={true}
-                    handleFiles={handleFiles}
-                  >
-                    <Avatar className="size-[160px]">
-                      <AvatarImage src={url} alt="Avatar Placeholder" />
-                      <AvatarFallback>
-                        <Camera className="size-[30px]" />
-                      </AvatarFallback>
-                    </Avatar>
-                  </ReactFileReader> */}
+                  <img
+                    className="size-[160px] rounded-full absolute
+                  "
+                    src={imageUrl as string}
+                  />
                 </div>
-
-                {/* <div className="flex flex-col space-y-1.5">
-                  <div className="size-[160px] flex relative justify-center items-center ">
-                    <Camera className="size-[40px] absolute" />
-                    <input
-                      type="file"
-                      className="size-[160px] rounded-full border-dashed border-[1px]"
-                    ></input>
-                  </div>
-                </div> */}
               </FormControl>
 
               <FormMessage />
