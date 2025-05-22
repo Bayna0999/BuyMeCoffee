c9826279d2599ce75b706ccd0129c1ed3f256bd4
24LP6733 24LP6733 <24LP6733@G302-CC0097.local>
2025-05-22 09:33:58 +0800

    hh

--- frontend/src/components/dashboard/Creaters.tsx
+++ frontend/src/components/dashboard/Creaters.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SquareArrowOutUpRight } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import React from "react";
+import { SquareArrowOutUpRight } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 export const Creaters = () => {
   return (
     <div className="flex flex-col w-full h-fit px-6 py-6  gap-4 border-[2px] rounded-md">
@@ -14,12 +14,12 @@ export const Creaters = () => {
             <p className="text-[20px] font-bold">hello</p>
           </div>
         </div>
-        <div className="flex px-4 py-2 h-[40px] w-fit text-[14px] gap-2">
+        <div className="flex px-4 py-2 h-[40px] w-fit text-[14px] gap-2 bg-[#F4F4F5] rounded-md justify-center items-center">
           <p>View profile</p> <SquareArrowOutUpRight className="size-4" />
         </div>
       </div>
       <div className="flex gap-5">
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-[50%]">
           <p className="text-[16px] font-bold">About Space ranger</p>
           <p className="text-[12px] ">
             Thank you for being so awesome everyday! You always manage to
@@ -27,7 +27,7 @@ export const Creaters = () => {
             much money it’s all I can contribute at the moment
           </p>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-[50%]">
           <p className="text-[16px] font-bold">Social media URL</p>
           <p className="text-[12px] ">
             https://buymeacoffee.com/baconpancakes1
